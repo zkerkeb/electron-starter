deed1e727b9b9ccaac906f6342b31a95ce12a60f
Zakaria Kerkeb <zkerkeb@student.42.fr>
2023-02-02 13:07:17 +0100

    progress

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,14 @@
 // Module to control the application lifecycle and the native browser window.
-const { app, BrowserWindow, protocol, ipcMain, contextBridge, ipcRenderer  } = require("electron");
-const path = require("path");
-const url = require("url");
+const {
+  app,
+  BrowserWindow,
+  protocol,
+  ipcMain,
+  contextBridge,
+  ipcRenderer
+} = require('electron')
+const path = require('path')
+const url = require('url')
 
 // Create the native browser window.
 function createWindow() {
@@ -11,41 +18,39 @@ function createWindow() {
     // Set the path of an additional "preload" script that can be used to
     // communicate between node-land and browser-land.
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
   /* Here is the explanation for the code above:
 1. The ipcMain.on() method listens to the 'get-system-info' event.
 2. It then replies to that event with the 'system-info' event and passes some data in the form of an object.
 3. The process.platform and process.version properties are used to get the platform and version of the operating system. */
-ipcMain.on('get-system-info', (event) => {
-  event.reply('system-info', {
-    platform: process.platform,
-    version: process.version, 
-    cpuUsage: process.getCPUUsage(),
-    memoryUsage: process.getSystemMemoryInfo(),
-    systemVersion: process.getSystemVersion(),
-  });
-});
-
-
+  ipcMain.on('get-system-info', event => {
+    event.reply('system-info', {
+      platform: process.platform,
+      version: process.version,
+      cpuUsage: process.getCPUUsage(),
+      memoryUsage: process.getSystemMemoryInfo(),
+      systemVersion: process.getSystemVersion()
+    })
+  })
 
   // In production, set the initial browser path to the local bundle generated
   // by the Create React App build process.
   // In development, set it to localhost to allow live/hot-reloading.
   const appURL = app.isPackaged
     ? url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true,
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
       })
-    : "http://localhost:3000";
-  mainWindow.loadURL(appURL);
+    : 'http://localhost:3000'
+  mainWindow.loadURL(appURL)
 
   // Automatically open Chrome's DevTools in development mode.
   if (!app.isPackaged) {
-    mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools()
   }
 }
 
@@ -53,59 +58,55 @@ ipcMain.on('get-system-info', (event) => {
 // them from the local production bundle (e.g.: local fonts, etc...).
 function setupLocalFilesNormalizerProxy() {
   protocol.registerHttpProtocol(
-    "file",
+    'file',
     (request, callback) => {
-      const url = request.url.substr(8);
-      callback({ path: path.normalize(`${__dirname}/${url}`) });
+      const url = request.url.substr(8)
+      callback({ path: path.normalize(`${__dirname}/${url}`) })
     },
-    (error) => {
-      if (error) console.error("Failed to register protocol");
+    error => {
+      if (error) console.error('Failed to register protocol')
     }
-  );
+  )
 }
-
 
 // This method will be called when Electron has finished its initialization and
 // is ready to create the browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow();
-  setupLocalFilesNormalizerProxy();
- 
-  app.on("activate", function () {
-    
+  createWindow()
+  setupLocalFilesNormalizerProxy()
+
+  app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      createWindow()
     }
-  });
-});
+  })
+})
 
 // Quit when all windows are closed, except on macOS.
 // There, it's common for applications and their menu bar to stay active until
 // the user quits  explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
 // If your app has no need to navigate or only needs to navigate to known pages,
 // it is a good idea to limit navigation outright to that known scope,
 // disallowing any other kinds of navigation.
-const allowedNavigationDestinations = "https://my-electron-app.com";
-app.on("web-contents-created", (event, contents) => {
-  contents.on("will-navigate", (event, navigationUrl) => {
-    const parsedUrl = new URL(navigationUrl);
+const allowedNavigationDestinations = 'https://my-electron-app.com'
+app.on('web-contents-created', (event, contents) => {
+  contents.on('will-navigate', (event, navigationUrl) => {
+    const parsedUrl = new URL(navigationUrl)
 
     if (!allowedNavigationDestinations.includes(parsedUrl.origin)) {
-      event.preventDefault();
+      event.preventDefault()
     }
-  });
-});
+  })
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
-
--- public/preload.js
+++ public/preload.js
@@ -1,24 +1,21 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron')
 
 // As an example, here we use the exposeInMainWorld API to expose the browsers
 // and node versions to the main window.
 // They'll be accessible at "window.versions".
-process.once("loaded", () => {
-  contextBridge.exposeInMainWorld("versions", process.versions);
-  contextBridge.exposeInMainWorld(
-    'electron',
-    {
-      doThing: 42,
-      requestSystemInfo: () => ipcRenderer.send('get-system-info'),
-      getSystemInfo: (setState) => ipcRenderer.on('system-info', (event, info) => {
-      console.log("🚀 ~ file: preload.js:19 ~ getSystemInfo: ~ info", info)
-        
-        setState(info);
-        }),
-      cleanSystemInfo: () => ipcRenderer.removeAllListeners('system-info')
-    } 
-  )
-  
-});
+process.once('loaded', () => {
+  contextBridge.exposeInMainWorld('versions', process.versions)
+  contextBridge.exposeInMainWorld('electron', {
+    doThing: 42,
+    requestSystemInfo: () => ipcRenderer.send('get-system-info'),
+    getSystemInfo: setState =>
+      ipcRenderer.on('system-info', (event, info) => {
+        console.log('🚀 ~ file: preload.js:19 ~ getSystemInfo: ~ info', info)
+
+        setState(info)
+      }),
+    cleanSystemInfo: () => ipcRenderer.removeAllListeners('system-info')
+  })
+})
